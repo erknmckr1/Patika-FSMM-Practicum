2fc8c5c65cd4c7cd2d74cbb3307a84007b4050dd
Erkan Mustafa Cakır <erknm.ckr@gmail.com>
2023-04-24 14:45:33 +0300

    ...

--- book-search-app/book-search/pages/detail/[id].jsx
+++ book-search-app/book-search/pages/detail/[id].jsx
@@ -1,50 +1,53 @@
 import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Link from "next/link";
 import Header from "@/components/Header/Header";
-import {BiArrowBack} from 'react-icons/bi'
+import { BiArrowBack } from "react-icons/bi";
 import Footer from "@/components/Footer/Footer";
 function index() {
-  
   const router = useRouter();
   const data = useSelector((state) => state.books.data);
   // query parametresını yakaladık ve datadan tıkladıgımız elemanı
   const { id } = router.query;
   const filteredData = data.find((book) => book.id === id);
 
-  // bir öncek sayfaya gitme ıcın 
-  const handleBack = () =>{
+  // bir öncek sayfaya gitme ıcın
+  const handleBack = () => {
     router.back();
-  }
+  };
 
- 
   return (
     <div className="h-screen">
-      <Header/>
+      <Header />
       <div className=" w-screen h-full flex flex-col justify-center items-center py-3">
-      <h3>TİTLE</h3>
+        <h3>TİTLE</h3>
         {filteredData && (
-          
           <div className="mt-[3.5rem] w-[90%] sm:w-2/3  border-2 flex items-center justify-center bg-white border-black rounded-3xl booksShadow overflow-hidden relative mb-[2.5rem] ">
-            <div className="w-1/3 h-full flex flex-col justify-center items-center border-2  ">
-              <div className=" w-full  h-1/3 m-2">
-                <Image
-                  width={100}
-                  height={50}
-                  alt="none"
-                  src={filteredData.imageLinks.thumbnail}
-                  className="rounded-md w-full h-full  "
-                />
+            {filteredData.imageLinks && (
+              <div className="w-1/3 h-full flex flex-col justify-center items-center border-2  ">
+                <div className=" w-full  h-1/3 m-2">
+                  <Image
+                    width={100}
+                    height={50}
+                    alt="none"
+                    src={filteredData.imageLinks.thumbnail}
+                    className="rounded-md w-full h-full  "
+                  />
+                </div>
+                <button className="text:[15px] sm:text-[25px] underline hover:text-blue-500">
+                  <Link target="_blank" href={filteredData.previewLink}>
+                    Preview Book
+                  </Link>
+                </button>
               </div>
-              <button className="text:[15px] sm:text-[25px] underline hover:text-blue-500">
-                <Link target="_blank" href={filteredData.previewLink}>
-                  Prewiev Book
-                </Link>
-              </button>
-            </div>
-            <div className="w-2/3 h-full border-2  overflow-x-auto p-3 ">
+            )}
+            <div
+              className={`w-${
+                filteredData.imageLinks ? "2/3" : "full"
+              } h-full border-2  overflow-x-auto p-3 `}
+            >
               <p>
                 <span className="title">Author:</span>{" "}
                 {filteredData.authors && filteredData.authors[0]}
@@ -66,15 +69,19 @@ function index() {
                 {filteredData && filteredData.description}{" "}
               </p>
             </div>
-            <div  onClick={handleBack}  className="cursor-pointer absolute top-2 left-0  sm:top-4 sm:right-32 hover:text-blue-700 flex items-center" >
-            <button className="text-[25px]"><BiArrowBack/></button>
-            <span className="text-[10px]">previous page</span>
+            <div
+              onClick={handleBack}
+              className="cursor-pointer absolute top-2 left-0  sm:top-4 sm:right-32 hover:text-blue-700 flex items-center"
+            >
+              <button className="text-[25px]">
+                <BiArrowBack />
+              </button>
+              <span className="text-[10px]">previous page</span>
             </div>
-            
           </div>
         )}
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
